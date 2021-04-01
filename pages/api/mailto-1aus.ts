8aa72e1cf008137d35f208148a13792351fcb801
Ronny Krammer <ronny.krammer@gwvs.de>
2021-04-01 23:00:07 +0200

    Next API redirect test

--- pages/api/mailto-1aus.ts
+++ pages/api/mailto-1aus.ts
@@ -6,5 +6,6 @@ type Data = {
 
 export default (req: NextApiRequest, res: NextApiResponse<Data>) => {
   res.setHeader('Location', 'mailto:1.ausdauer@sv-donaustauf.de');
+  res.redirect(307, 'mailto:1.ausdauer@sv-donaustauf.de');
   res.end(res.getHeader('Location'));
 }
--- pages/api/mailto-1vs.ts
+++ pages/api/mailto-1vs.ts
@@ -6,5 +6,6 @@ type Data = {
 
 export default (req: NextApiRequest, res: NextApiResponse<Data>) => {
   res.setHeader('Location', 'mailto:1.vorstand@sv-donaustauf.de');
+  res.redirect(307, 'mailto:1.vorstand@sv-donaustauf.de');
   res.end(res.getHeader('Location'));
 }
