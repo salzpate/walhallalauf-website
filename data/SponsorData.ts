726d98013be9d5962790824571e9136db8bf9d39
Ronny Krammer <ronny.krammer@gwvs.de>
2022-05-11 07:29:14 +0200

    change lattus image

--- data/SponsorData.ts
+++ data/SponsorData.ts
@@ -2,42 +2,42 @@ import { SponsorGalleryItems } from '@/components/SponsorGallery/SponsorGallery'
 
 export const SponsorData: SponsorGalleryItems[] = [
   {
-    imageUrl: '/assets/images/sponsoring/raiba.png',
+    imageUrl: '/assets/images/sponsoring/raiba-2022.png',
     link: 'https://www.rb-os.de',
     name: 'Raiffeisenbank Oberpfalz Süd eG',
   },
   {
-    imageUrl: '/assets/images/sponsoring/kneitinger.jpg',
+    imageUrl: '/assets/images/sponsoring/kneitinger-2022.jpg',
     link: 'https://www.kneitinger.de',
     name: 'Brauerei Kneitinger',
   },
   {
-    imageUrl: '/assets/images/sponsoring/lattus.jpg',
+    imageUrl: '/assets/images/sponsoring/lattus-2022.jpg',
     link: 'https://www.edeka.de/eh/s%C3%BCdbayern/edeka-lattus-regensburger-stra%C3%9Fe-15/index.jsp',
     name: 'EDEKA Lattus',
   },
   {
-    imageUrl: '/assets/images/sponsoring/thurnundtaxis-apotheke.jpg',
+    imageUrl: '/assets/images/sponsoring/thurnundtaxis-apotheke-2022.jpg',
     link: 'https://www.apotheke-donaustauf.de',
     name: 'Thurn und Taxis Apotheke Donaustauf',
   },
   {
-    imageUrl: '/assets/images/sponsoring/athen-donaustauf.jpg',
+    imageUrl: '/assets/images/sponsoring/athen-donaustauf-2022.jpg',
     link: 'https://athen-donaustauf.business.site/',
     name: 'Restaurant Athen Donaustauf',
   },
   {
-    imageUrl: '/assets/images/sponsoring/aloha30athletics.png',
+    imageUrl: '/assets/images/sponsoring/aloha30athletics-2022.png',
     link: 'http://www.marionsummerer.com',
     name: 'Marion Summerer',
   },
   {
-    imageUrl: '/assets/images/sponsoring/alexmueller.png',
+    imageUrl: '/assets/images/sponsoring/alexmueller-2022.png',
     link: 'https://al-mueller.de',
     name: 'Bureau 2+ Alex Müller',
   },
   {
-    imageUrl: '/assets/images/sponsoring/tt.png',
+    imageUrl: '/assets/images/sponsoring/tt-2022.png',
     link: 'https://forst.thurnundtaxis.de',
     name: 'Thurn und Taxis Forst GmbH & Co. KG',
   },
