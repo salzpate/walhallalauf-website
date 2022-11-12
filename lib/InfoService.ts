3af86792994d5f1766d4a25e81734667522c2603
Ronny Krammer <ronny.krammer@gwvs.de>
2022-11-12 13:11:53 +0100

    Add API_TOKEN

--- lib/InfoService.ts
+++ lib/InfoService.ts
@@ -1,11 +1,18 @@
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 
 class InfoService {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   getInfos = async (): Promise<AxiosResponse<any, any>> => {
     const baseUrl = process.env.STRAPI_API_URL as string;
+    const apiToken = process.env.STRAPI_API_TOKEN as string;
 
-    return axios.get(`${baseUrl}/infos`);
+    const config: AxiosRequestConfig = {
+      headers: {
+        Authorization: 'Bearer ' + apiToken,
+      },
+    };
+
+    return axios.get(`${baseUrl}/infos`, config);
   };
 }
 
