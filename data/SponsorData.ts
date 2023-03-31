4349e70061d0d95174120a9d00590c98a1484c4c
Ronny Krammer <ronny.krammer@gwvs.de>
2023-03-31 08:26:30 +0200

    Sponsorliste

--- data/SponsorData.ts
+++ data/SponsorData.ts
@@ -12,6 +12,11 @@ export const SponsorData: SponsorGalleryItems[] = [
     name: 'Brauerei Kneitinger',
   },
   {
+    imageUrl: '/assets/images/sponsoring/killmann-2023.png',
+    link: 'https://www.maler-killmann.de',
+    name: 'Malerbetrieb Killmann',
+  },
+  {
     imageUrl: '/assets/images/sponsoring/lattus-2022.jpg',
     link: 'https://www.edeka.de/eh/s%C3%BCdbayern/edeka-lattus-regensburger-stra%C3%9Fe-15/index.jsp',
     name: 'EDEKA Lattus',
@@ -27,11 +32,6 @@ export const SponsorData: SponsorGalleryItems[] = [
     name: 'Restaurant Athen Donaustauf',
   },
   {
-    imageUrl: '/assets/images/sponsoring/aloha30athletics-2022.png',
-    link: 'http://www.marionsummerer.com',
-    name: 'Marion Summerer',
-  },
-  {
     imageUrl: '/assets/images/sponsoring/alexmueller-2022.png',
     link: 'https://al-mueller.de',
     name: 'Bureau 2+ Alex Müller',
@@ -45,5 +45,30 @@ export const SponsorData: SponsorGalleryItems[] = [
     imageUrl: '/assets/images/sponsoring/armenspital-2022.png',
     link: 'https://www.historisches-armen-spital.de',
     name: 'Historisches Armen Spital',
+  },
+  {
+    imageUrl: '/assets/images/sponsoring/santorini-2023.png',
+    link: 'https://santorini-donaustauf.de',
+    name: 'Santorini',
+  },
+  {
+    imageUrl: '/assets/images/sponsoring/bella-napoli-2023.jpg',
+    link: 'http://trattoria-bella-napoli.de',
+    name: 'Trattoria Bella Napoli',
+  },
+  {
+    imageUrl: '/assets/images/sponsoring/blumenhandl-2023.png',
+    link: 'https://www.blumenhandl.de',
+    name: 'Blumen Handl',
+  },
+  {
+    imageUrl: '/assets/images/sponsoring/tatlis-2023.png',
+    link: 'https://tatlis.de',
+    name: 'Tatlis',
+  },
+  {
+    imageUrl: '/assets/images/sponsoring/casa-dei-matti-2023.webp',
+    link: 'https://www.la-casa-dei-matti.de',
+    name: 'La Casa dei Matti',
   },
 ];
