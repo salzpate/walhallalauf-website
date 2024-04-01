c7b045e56b6f30c8fae72194d74be9a8ef25e407
Ronny Krammer <ronny.krammer@gwvs.de>
2024-04-01 09:08:07 +0200

    test

--- util/sendEmail.ts
+++ util/sendEmail.ts
@@ -5,8 +5,8 @@ export const sendEmail = async (from: string, text: string): Promise<[ClientResp
   const sgMail = new MailService();
   sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
   return sgMail.send({
-    to: 'svd-ausdauer@web.de',
-    from: 'Walhallalauf-Homepage <svd-ausdauer@web.de>',
+    to: 'orga@walhallalauf.de',
+    from: 'Walhallalauf-Homepage <orga@walhallalauf.de>',
     replyTo: from,
     subject: 'E-Mail Anfrage aus der Walhallalauf-Homepage',
     text: text,
