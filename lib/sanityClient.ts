6acdc54740345402f7373d8a0708f550680e344f
Ronny Krammer <ronny.krammer@gwvs.de>
2025-03-04 18:32:36 +0100

    test

--- lib/sanityClient.ts
+++ lib/sanityClient.ts
@@ -1,8 +1,8 @@
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-  projectId: "nr3qhqby",
-  dataset: "production",
+  projectId: process.env.SANITY_PROJECT_ID,
+  dataset: process.env.SANITY_DATASET,
   apiVersion: "2025-03-04",
   useCdn: false,
 });
