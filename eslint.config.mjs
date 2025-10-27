da069e48cbd1f49e0c3d97b8d9a7faf7ff903fa0
Ronny Krammer <ronny.krammer@gwvs.de>
2025-10-27 22:03:17 +0100

    lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig, globalIgnores } from 'eslint/config'
+import reactPlugin from "eslint-plugin-react";
 import nextVitals from 'eslint-config-next/core-web-vitals'
 import nextTs from 'eslint-config-next/typescript'
 import prettier from 'eslint-config-prettier';
-import reactPlugin from 'eslint-plugin-react';
 
 const eslintConfig = defineConfig([
   globalIgnores([
@@ -19,7 +19,7 @@ const eslintConfig = defineConfig([
   ...nextTs,
   prettier,
   {
-    extends: [reactPlugin.configs.flat.recommended, reactPlugin.configs.flat['jsx-runtime']],
+    extends: [reactPlugin.configs.flat.recommended],
     rules: {
       'import/extensions': 'off',
       'jsx-a11y/anchor-is-valid': 'off',
